7f053392dc5be8ac80466a58c67eca17854ff660
Marius-Alexandru MANOLACHE (115887) <marius.manolache01@stud.acs.upb.ro>
2023-04-25 23:05:27 +0300

    removed the receivers for methods, added some new methods to db

---
+++ backend/back-end/cmd/api/data-models.go
@@ -0,0 +1,57 @@
+package main
+
+type requestPayload struct {
+	Action                 string                 `json:"action"`
+	SchoolEntry            schoolEntry            `json:"schoolEntry,omitempty"`
+	RegisterServicePayload registerServicePayload `json:"registerServicePayload,omitempty"`
+}
+
+type schoolEntry struct {
+	Name        string       `json:"name"`
+	Departments []department `json:"departments"`
+}
+
+type department struct {
+	Name         string        `json:"name"`
+	Teachers     []string      `json:"teachers"`
+	YearsOfStudy []yearOfStudy `json:"yearsOfStudy"`
+}
+
+type yearOfStudy struct {
+	Type             string   `json:"type"` // bachelor, master, phd
+	Year             int      `json:"year"`
+	NumberOfSerieses int      `json:"numberOfSerieses"`
+	NumberOfGroups   int      `json:"numberOfGroups"`
+	Subjects         []string `json:"subjects"`
+}
+
+// --------------------------------------------------------------------------------------------
+
+type registerServicePayload struct {
+	Role    string      `json:"role"` // student, teacher, admin
+	Student student     `json:"student,omitempty"`
+	Other   otherPerson `json:"other,omitempty"` // maybe in the future
+}
+
+type student struct {
+	FirstName  string `json:"firstName"`
+	MiddleName string `json:"middleName,omitempty"`
+	LastName   string `json:"lastName"`
+
+	Email string `json:"email"`
+
+	Faculty    string `json:"faculty"`
+	Department string `json:"department"`
+	Year       int    `json:"year"`
+	Series     string `json:"series"`
+	Group      string `json:"group"`
+
+	Regiser register `json:"register"`
+}
+
+type register struct {
+	// maybe it would be better to keep this somewhere else
+	// a service for people data and another one for register(catalog) data
+}
+
+type otherPerson struct{} // maybe in the future
