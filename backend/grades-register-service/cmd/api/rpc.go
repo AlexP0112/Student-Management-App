c3cdac8457658e9de886d2317dc65890c560d6d9
stefandaius <stefan.iordache2001@gmail.com>
2023-05-29 14:30:06 +0300

    grades and personal data

--- backend/grades-register-service/cmd/api/rpc.go
+++ backend/grades-register-service/cmd/api/rpc.go
@@ -1,6 +1,43 @@
 package main
 
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"grades-register-service/data"
+)
+
 type RPCServer struct{}
 
 type RPCPayload struct {
+}
+
+func (r *RPCServer) getAllGrades(reply *[]data.StudentRegister) error {
+	var grades []data.StudentRegister
+	collection := mongoClient.Database("university").Collection("grades")
+	cursor, err := collection.Find(context.Background(), bson.M{})
+	if err != nil {
+		return err
+	}
+	err = cursor.All(context.Background(), &grades)
+	if err != nil {
+		return err
+	}
+	*reply = grades
+	return nil
+}
+
+func (r *RPCServer) getGradesByCNP(payload string, reply *[]data.StudentRegister) error {
+	var grades []data.StudentRegister
+	collection := mongoClient.Database("university").Collection("grades")
+	filter := bson.M{"cnp": payload}
+	cursor, err := collection.Find(context.Background(), filter)
+	if err != nil {
+		return err
+	}
+	err = cursor.All(context.Background(), &grades)
+	if err != nil {
+		return err
+	}
+	*reply = grades
+	return nil
 }
--- backend/personal-data-service/cmd/api/rpc.go
+++ backend/personal-data-service/cmd/api/rpc.go
@@ -1,6 +1,65 @@
 package main
 
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"personal-data-service/data"
+)
+
 type RPCServer struct{}
 
 type RPCPayload struct {
+}
+
+func (r *RPCServer) getAllPersonalData(reply *[]data.PersonalInfo) error {
+	var personalInfo []data.PersonalInfo
+	collection := mongoClient.Database("university").Collection("personalData")
+	cursor, err := collection.Find(context.Background(), bson.M{})
+	if err != nil {
+		return err
+	}
+	err = cursor.All(context.Background(), &personalInfo)
+	if err != nil {
+		return err
+	}
+	*reply = personalInfo
+	return nil
+}
+
+func (r *RPCServer) getPersonalDataByCNP(payload string, reply *data.PersonalInfo) error {
+	var personalInfo data.PersonalInfo
+	collection := mongoClient.Database("university").Collection("personalData")
+	filter := bson.M{"cnp": payload}
+	err := collection.FindOne(context.Background(), filter).Decode(&personalInfo)
+	if err != nil {
+		return err
+	}
+	*reply = personalInfo
+	return nil
+}
+
+func (r *RPCServer) getPersonalDataByFirstName(payload string, reply *[]data.PersonalInfo) error {
+	var personalInfo []data.PersonalInfo
+	collection := mongoClient.Database("university").Collection("personalData")
+	filter := bson.M{"firstName": payload}
+	cursor, err := collection.Find(context.Background(), filter)
+	if err != nil {
+		return err
+	}
+	err = cursor.All(context.Background(), &personalInfo)
+	if err != nil {
+		return err
+	}
+	*reply = personalInfo
+	return nil
+}
+
+func (r *RPCServer) addPersonalData(payload data.PersonalInfo, reply *string) error {
+	collection := mongoClient.Database("university").Collection("personalData")
+	_, err := collection.InsertOne(context.Background(), payload)
+	if err != nil {
+		return err
+	}
+	*reply = "OK"
+	return nil
 }
